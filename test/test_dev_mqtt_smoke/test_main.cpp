466a18a02202f6e68d391069b196e025e4ea572d
hjca14 <hjca14@gmail.com>
2026-08-22 16:28:35 -0300

    test(dev): remove obsolete smoke constants

--- test/test_dev_mqtt_smoke/test_main.cpp
+++ test/test_dev_mqtt_smoke/test_main.cpp
@@ -6,20 +6,17 @@ namespace { constexpr const char* kId = "ib-dev-test-placeholder"; }
 void setUp() {}
 void tearDown() {}
 
-void test_dev_contract_topics_client_id_and_delivery_options() {
+void test_dev_contract_topics_and_client_id() {
     MqttTopics topics(devMqttTopicsConfig(kId));
     TEST_ASSERT_EQUAL_STRING(kId, MqttTopics::clientId(kId).c_str());
     TEST_ASSERT_EQUAL_STRING("interbridge/ib-dev-test-placeholder/commands", topics.commands().c_str());
     TEST_ASSERT_EQUAL_STRING("$aws/rules/interbridge_dev_response_rule/interbridge/ib-dev-test-placeholder/responses", topics.responsesIngest().c_str());
     TEST_ASSERT_EQUAL(std::string::npos, topics.commands().find('#'));
     TEST_ASSERT_EQUAL(std::string::npos, topics.commands().find('+'));
-    TEST_ASSERT_EQUAL(1, kDevSmokeCommandSubscribeQos);
-    TEST_ASSERT_EQUAL(1, kDevSmokeResponsePublishQos);
-    TEST_ASSERT_FALSE(kDevSmokeRetain);
 }
 
 int main(int, char**) {
     UNITY_BEGIN();
-    RUN_TEST(test_dev_contract_topics_client_id_and_delivery_options);
+    RUN_TEST(test_dev_contract_topics_and_client_id);
     return UNITY_END();
 }
